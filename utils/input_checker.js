480fc815974501af03eb76d8b94edc376671ba18
Ruben Lousada <rlousada123456@gmail.com>
2022-02-08 02:10:20 +0000

    Restruct project, verifying input

--- utils/input_checker.js
+++ utils/input_checker.js
@@ -4,6 +4,8 @@ function checkPassword(password, username) {
     if (password.length < constants.PASSWORD_LENGTH) return constants.PASSWORD_TOO_SMALL;
 
     if (password.includes(username)) return constants.PASSWORD_CONTAINS_USERNAME;
+
+    console.log(password.includes(username));
 }
 
 function checkEmail(email) {
