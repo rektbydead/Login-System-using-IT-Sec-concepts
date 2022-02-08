d7396542d4e5cd53868dd0f604542bd916f9859b
Ruben Lousada <rlousada123456@gmail.com>
2022-02-08 13:02:13 +0000

    Change numbers to constants and now iterates x times

--- utils/encrypt.js
+++ utils/encrypt.js
@@ -1,10 +1,13 @@
 const crypto = require('crypto');
 const sql = require('./sql');
+const constants = require('./constants');
+
 
 const hashPassword = (password, salt) => {
     if (!salt) salt = createSalt();
 
-    return crypto.createHmac('sha256', salt).update(password).digest('hex'); 
+    // Salt the password "constants.ITERATIONS" times, "constants.LENGTH" length with "constants.ALGORITHM"
+    return crypto.pbkdf2Sync(password, salt, constants.ITERATIONS, constants.LENGTH, constants.ALGORITHM).toString('hex');
 }
 
 const createSalt = (size) => {
@@ -13,11 +16,20 @@ const createSalt = (size) => {
     return crypto.randomBytes(size).toString('hex').slice(0, size);
 }
 
+const verifyPassword = (hashedPassword, password, salt) => {
+    // Salt the password "constants.ITERATIONS" times, 64 length with "constants.ALGORITHM"
+    const newHash = crypto.pbkdf2Sync(password, salt, constants.ITERATIONS, constants.LENGTH, constants.ALGORITHM).toString('hex');
+    
+    // Verify password
+    return hashedPassword == newHash;
+}
+
 function getRandomNumber() {
     return Math.floor(Math.random() * sql.SALT_MAX_SIZE) + sql.SALT_MIN_SIZE;
 }
 
 module.exports = {
     hashPassword,
-    createSalt
+    createSalt,
+    verifyPassword
 };
