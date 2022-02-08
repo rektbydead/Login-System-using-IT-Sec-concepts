114d6cca1b1ff5a9dc841a227b14c560d1461cd3
Ruben Lousada <rlousada123456@gmail.com>
2022-02-08 12:12:17 +0000

    Add comments

--- routes/signup.js
+++ routes/signup.js
@@ -7,29 +7,32 @@ const inputChecker = require('../utils/input_checker');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
+    // Body is empty
     let body = req.body;
-
     if (!Object.keys(body).length) return res.send("Empty body");   
 
     let username = body.username;
     let email = body.email ? String(body.email).toLowerCase() : undefined;
     let password = body.password;
 
+    // Something is missing
     if (!username) return res.status(400).send(constants.USERNAME_IS_MISSING);
     if (!email) return res.status(400).send(constants.EMAIL_IS_MISSING);
     if (!password) return res.status(400).send(constants.PASSWORD_IS_MISSING);
 
+    // Input check
     if (!inputChecker.checkEmail(email)) return res.status(400).send(constants.EMAIL_IS_NOT_VALID);
     if (await sql.checkUserExists(email)) return res.status(400).send(constants.EMAIL_ALREADY_EXITS);
 
     let passwordCheck = inputChecker.checkPassword(password, username);
     if (passwordCheck) return res.status(400).send(passwordCheck);
 
+    // Create Salt
     let salt = encrypt.createSalt();
     let hashedPassword = encrypt.hashPassword(password, salt);
 
+    // Add user and report success
     sql.addUser(username, email, hashedPassword, salt);
-
     res.status(200).send(constants.USER_ADD_SUCCESS);
 });
 
