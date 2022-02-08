718851d5483ce89bc192f07772998b10d0ca1244
Ruben Lousada <rlousada123456@gmail.com>
2022-02-08 12:11:50 +0000

    Start login.js

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const sql = require('../utils/sql');
+const constants = require('../utils/constants');
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
+router.get('/', async function(req, res) {
+    // Body is empty
+    let body = req.body;
+    if (!Object.keys(body).length) return res.send("Empty body");    
 
+    let email = body.email ? String(body.email).toLowerCase() : undefined;
+    let password = body.password;
+
+    // User does not exists
+    let result = await sql.getLoginInformation(email);
+    if (!result) return res.send(constants.EMAIL_PASSWORD_NOT_CORRECT);
+
+    console.log(result);
+    
 });
 
 module.exports = router;
