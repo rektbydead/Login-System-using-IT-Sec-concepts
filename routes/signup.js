662e8ceed710325b9e5124ab0e6c54ca71c03826
Ruben Lousada <rlousada123456@gmail.com>
2022-02-08 00:48:25 +0000

    Adding user to db

--- routes/signup.js
+++ routes/signup.js
@@ -5,24 +5,25 @@ const sql = require('../sql');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    if (!Object.keys(req.body).length) {
+    let body = body;
+
+    if (!Object.keys(body).length) {
         return res.send("Empty body");   
     }
 
-    if (!req.body.username) return res.status(400).send("Username is missing");
-    if (!req.body.email) return res.status(400).send("Email is missing");
-    if (!req.body.password) return res.status(400).send("Password is missing");
+    if (!body.username) return res.status(400).send("Username is missing");
+    if (!body.email) return res.status(400).send("Email is missing");
+    if (!body.password) return res.status(400).send("Password is missing");
 
     let salt = encrypt.createSalt();
-    let hashedPassword = encrypt.hashPassword(req.body.password, salt);
-    console.log(hashedPassword);
+    let hashedPassword = encrypt.hashPassword(body.password, salt);
 
-    let userExists = await sql.checkUserExists(req.body.email);
-    if (userExists._results.length == 0) {
+    let userExists = await sql.checkUserExists(body.email);
+    if (userExists._results.length != 0) {
         return;
     }
 
-    console.log(2);
+    await sql.addUser(body.username, body.email, body.password, salt);
 
     res.send(hashedPassword);
 });
