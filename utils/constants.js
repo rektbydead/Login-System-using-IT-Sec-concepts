86d68e009594e5b97d3918ca59fc3969b3ed6900
Ruben Lousada <rlousada123456@gmail.com>
2022-02-08 02:28:37 +0000

    Complete SignUp (hash + salt), TODO: Pepper

--- utils/constants.js
+++ utils/constants.js
@@ -3,7 +3,8 @@ module.exports = {
     PASSWORD_LENGTH: 8,
 
     EMAIL_MATCH: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-
+    ONLY_LETTERS_MATCH: /^[A-Za-z]+$/,
+    ONLY_LETTERS_NUMBERS_MATCH: /^[^a-zA-Z0-9]+$/,
 
     // error constants
     PASSWORD_IS_MISSING: "Password is missing",
@@ -12,6 +13,7 @@ module.exports = {
 
     PASSWORD_TOO_SMALL: "Password has less than 8 characters",
     PASSWORD_HAS_NO_DIGIT: "Password has to have at least one digit",
+    PASSWORD_HAS_NO_SPECIAL_DIGIT: "Password has to have at least one special digit",
     PASSWORD_CONTAINS_USERNAME: "Password cannot contain username",
 
     EMAIL_IS_NOT_VALID: "Email is invalid",
--- utils/input_checker.js
+++ utils/input_checker.js
@@ -1,13 +1,22 @@
 const constants = require('./constants');
 
 function checkPassword(password, username) {
+    // Smaller than PASSWORD_LENGTH (8)
     if (password.length < constants.PASSWORD_LENGTH) return constants.PASSWORD_TOO_SMALL;
 
+    // Contains only letters
+    if (password.match(constants.ONLY_LETTERS_MATCH)) return constants.PASSWORD_HAS_NO_DIGIT;
+
+    // Contains only letters and numbers
+    if (password.match(constants.ONLY_LETTERS_NUMBERS_MATCH)) return constants.PASSWORD_HAS_NO_SPECIAL_DIGIT;
+
+    // Password contains username
     if (password.includes(username)) return constants.PASSWORD_CONTAINS_USERNAME;
 }
 
 function checkEmail(email) {
-    return String(email).toLowerCase().match(constants.EMAIL_MATCH);
+    //Email is valid
+    return email.match(constants.EMAIL_MATCH);
 }
 
 module.exports = {
