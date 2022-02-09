1d7be8eed8fb3e37054f8aca10dbf7542508bb35
Ruben Lousada <rlousada123456@gmail.com>
2022-02-09 00:27:06 +0000

    Do change_password & add coments

--- routes/change_password.js
+++ routes/change_password.js
@@ -1,14 +1,46 @@
 const express = require('express');
+const encrypt = require('../utils/encrypt');
+const constants = require('../utils/constants');
+const inputChecker = require('../utils/input_checker');
 
 const router = express.Router();
 
-router.route('/')
-    .get((req, res) => {
-        res.send('Get a random book')
-    })
-    .get((req, res) => {
-        res.send('Get a random book')
-    });
+router.put('/', async function(req, res) {
+    // Body is empty
+    let body = req.body;
+    if (!Object.keys(body).length) return res.send("Empty body");    
+
+    let email = body.email.toLowerCase();
+    let password = body.password;
+    let newPassword = body.new_password;
+
+    // Something is missing
+    if (!email) return res.status(400).send(constants.EMAIL_IS_MISSING);
+    if (!password) return res.status(400).send(constants.PASSWORD_IS_MISSING);
+    if (!newPassword) return res.status(400).send(constants.NEW_PASSWORD_IS_MISSING);
+
+    // Input check
+    if (!inputChecker.checkEmail(email)) return res.status(400).send(constants.EMAIL_IS_NOT_VALID);
+
+    // Check Password
+    let passwordCheck = inputChecker.checkPassword(newPassword, username);
+    if (passwordCheck) return res.status(400).send(passwordCheck);
+
+    // User does not exists
+    let result = await sql.getLoginInformation(email);
+    if (!result) return res.send(constants.EMAIL_PASSWORD_NOT_CORRECT);
+
+    // Verify password is correct
+    if (!encrypt.verifyPassword(result.password, password, result.salt)) return res.send(constants.EMAIL_PASSWORD_NOT_CORRECT);
+
+    // Create new Salt password
+    let salt = encrypt.createSalt();
+    let hashedPassword = encrypt.hashPassword(password, salt);
+    
+    // Update value in database
+
+    res.status(200).send(constants.USER_ADD_SUCCESS);
+});
 
 
 module.exports = router;
